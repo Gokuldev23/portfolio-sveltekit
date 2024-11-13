afeeb630068bc6ab1c228f40baf394c2063f72df
Gokuldev23 <gkforgeneral@gamil.com>
2024-11-13 12:05:26 +0530

    initial commit

---
+++ src/routes/+layout.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import '../app.css';
+
+	let { children } = $props();
+</script>
+
+
+<div class="bg-blue-400 p-4">
+	{@render children()}
+</div>
---
+++ src/routes/+page.svelte
@@ -0,0 +1,10 @@
+<script>
+
+</script>
+
+
+
+<main class="flex w-full">
+    <!-- <img src="/myImage.png" alt="">
+    <img class="shrink-0 self-start" src="/myImage1.png" alt=""> -->
+</main>
