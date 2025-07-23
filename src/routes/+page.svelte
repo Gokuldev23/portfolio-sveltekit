737661133e166b41d3f6370cc60b337c6312e2ef
Gokuldev23 <gkforgeneral@gmail.com>
2025-07-23 20:04:14 +0530

    Hero2 filename changed to Hero and About page deleted

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,9 +1,7 @@
 <script>
-	import About from '$lib/components/About.svelte';
 	import Hero from '$lib/components/Hero.svelte';
-	import Hero2 from '$lib/components/Hero2.svelte';
 </script>
 
 <section class="">
-	<Hero2 />
+	<Hero />
 </section>
