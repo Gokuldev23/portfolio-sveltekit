6552e11c01bc7698624d6155791c51af51dda319
Gokuldev23 <gkforgeneral@gmail.com>
2025-07-26 16:01:00 +0530

    fix: instead of hard coded projects array n, now it is categorized based on project type

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,7 +18,7 @@
 	let { children } = $props();
 </script>
 
-<div class="relative h-dvh  bg-black">
+<div class="relative min-h-dvh  bg-black">
 	<div class="fixed right-4 top-4 text-white flex gap-2 items-center bg-yellow-800 px-3 py-0.5 rounded-sm">
 		<div>
 			<Icon icon="arcticons:nrf-toolbox" />
