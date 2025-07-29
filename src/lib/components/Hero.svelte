319e6c1b7986a4887123b67fce969f097902cb47
Gokuldev23 <gkforgeneral@gmail.com>
2025-07-29 11:38:45 +0530

    modified: Project Type now sorted based on given order

--- src/lib/components/Hero.svelte
+++ src/lib/components/Hero.svelte
@@ -1,13 +1,19 @@
-<script>
+<script lang="ts">
 	import { projects } from '$lib/js/projects';
 	import About from './About.svelte';
 	import ProjectCatogory from './ProjectCatogory.svelte';
 
+	const order = {
+		FEATURED: 0,
+		PROFESSIONAL: 1,
+		EXPERIMENTAL: 2
+	} as const;
 
 	let grouped = Object.groupBy(projects, (item) => item.type);
 
-	let categories = Object.entries(grouped);
-
+	let categories = Object.entries(grouped).sort(
+		([a], [b]) => order[a as keyof typeof order] - order[b as keyof typeof order]
+	);
 </script>
 
 <div class=" bg-gray-950 px-4 py-10 text-white sm:px-10">
