c6410933f1b51e42db841fc9fe1e69e6d1aa03a6
Gokuldev23 <gkforgeneral@gmail.com>
2025-07-31 14:16:02 +0530

    routechanged: top level route should be un affected

---
+++ src/routes/(home)/+page.svelte
@@ -0,0 +1,24 @@
+<script lang="ts">
+	import ProjectCatogory from "$lib/components/ProjectCatogory.svelte";
+import { projects } from "$lib/js/projects";
+
+
+    const order = {
+		FEATURED: 0,
+		PROFESSIONAL: 1,
+		EXPERIMENTAL: 2
+	} as const;
+
+	let grouped = Object.groupBy(projects, (item) => item.type);
+
+	let categories = Object.entries(grouped).sort(
+		([a], [b]) => order[a as keyof typeof order] - order[b as keyof typeof order]
+	);
+
+</script>
+
+<main>
+    {#each categories as category}
+        <ProjectCatogory category={category}/>
+    {/each}
+</main>
