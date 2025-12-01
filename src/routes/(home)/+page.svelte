3c004dbaee8ce26e099b3db304cb8fd95d26fe58
Gokuldev23 <gkforgeneral@gmail.com>
2025-12-01 11:29:28 +0530

    EXPERIMENT: monitor custom button click on GA4;

--- src/routes/(home)/+page.svelte
+++ src/routes/(home)/+page.svelte
@@ -1,9 +1,8 @@
 <script lang="ts">
-	import ProjectCatogory from "$lib/components/ProjectCatogory.svelte";
-import { projects } from "$lib/js/projects";
+	import ProjectCatogory from '$lib/components/ProjectCatogory.svelte';
+	import { projects } from '$lib/js/projects';
 
-
-    const order = {
+	const order = {
 		FEATURED: 0,
 		PROFESSIONAL: 1,
 		EXPERIMENTAL: 2
@@ -14,11 +13,10 @@ import { projects } from "$lib/js/projects";
 	let categories = Object.entries(grouped).sort(
 		([a], [b]) => order[a as keyof typeof order] - order[b as keyof typeof order]
 	);
-
 </script>
 
 <main>
-    {#each categories as category}
-        <ProjectCatogory category={category}/>
-    {/each}
+	{#each categories as category}
+		<ProjectCatogory {category} />
+	{/each}
 </main>
